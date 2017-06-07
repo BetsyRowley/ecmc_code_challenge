a9bd7fe0cd7da4f8280a4a6285e11e3677b07ca8
Betsy Rowley <BRowley8@gmail.com>
2017-06-07 07:57:47 -0500

    Adds function

--- find_consecutive_runs.js
+++ find_consecutive_runs.js
@@ -1,1 +1,15 @@
-console.log('I\'m a consecutive run.');
+function findConesecutiveRuns(array) {
+  // console.log([1, 2, 3, 5, 10, 9, 8, 9, 10, 11, 7, 8, 7]);
+
+  var newArray = [];
+  for(var i = 0; i < array.length; i++) {
+    if((array[i] - arrary[i-1] === 1) || (array[i] - arrary[i-1] === -1)) {
+      newArray.push(TRUE);
+    } else {
+      newArray.push(FALSE);
+    }
+  }
+  console.log(newArray);
+}
+
+findConesecutiveRuns();
