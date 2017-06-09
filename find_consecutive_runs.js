6c7b89b0c69eee8efaaa07cac5d4a4bcbf86e0af
Betsy Rowley <BRowley8@gmail.com>
2017-06-08 19:18:33 -0500

    Finds consecutive runs based on passed argrument

--- find_consecutive_runs.js
+++ find_consecutive_runs.js
@@ -1,19 +1,5 @@
 // console.log(process.argv);
-var argument = (process.argv[2]).split('');
-var array = [1, 2, 3, 5, 10, 9, 8, 9, 10, 11, 7, 8, 7];
-
-// function makeArray() {
-//   for(var i = 0; i < array.length; i++) {
-//     if(array[i] != '[' || array[i] != ',' || array[i] != ' ' || array[i] != ']') {
-//       array.push(array[i]);
-//     } else {
-//       return 0;
-//     }
-//   }
-//   console.log(array);
-// }
-//
-// makeArray();
+var array = (process.argv[2]).replace("[","").replace("]","").split(',');
 console.log(array);
 console.log(typeof(array));
 console.log(array.length);
@@ -22,11 +8,11 @@ function findConsecutiveRuns() {
   var newArray = [];
   var run2 = [];
   var run3 = [];
-  
+
   for(var i = 0; i < array.length; i++) {
     run2 = array[i] - array[i + 1];
     run3 = array[i + 1] - array[i + 2];
-    if((run2 === 1 || run2 === -1) && run2 === run3) {
+    if((run2 === 1 || run2 === -1) && (run2 === run3)) {
         newArray.push(i);
       }
   }
